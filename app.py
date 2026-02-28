975b7b980b2288e151b8aaecbb090fc911e8be91
Hetvi Patel <hetvi1655@gmail.com>
2026-02-28 21:30:14 +0530

    Logo changes Done

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ SPONSORS = [
     {
         "name": "Rizvi Institute of Management & Research (RIMSR)",
         "role": "Venue Partner",
-        "image": "https://raw.githubusercontent.com/HetviPatel1655/WiDS-Mumbai/main/static/Rizvi%20LOGO.png",
+        "image": "https://raw.githubusercontent.com/HetviPatel1655/WiDS-Mumbai/main/static/Rizvi%20LOGO.jpeg",
         "description": "Our esteemed host partner for WiDS Mumbai 2026."
     }
 ]
